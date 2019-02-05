2b3bd53fb4ae3ee6bedafd8c29ae5dbd2788e14b
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2019-02-05 12:23:07 +0100

    deleted files from dist

--- dist/configuration/credentialsAzure.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    azureVisionConfig: {
-        azureEndPoint: "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/",
-        azureKey1: "247f1ba3a00b419384de6272ca6d5781",
-        azureKey2: "a9bb95cbb8e3417f9e5c4e0938268903",
-    },
-};
